8484a9db1a710f5d9ca61dac5ff2e7bb00cc3fbf
Stephen Cross <stephen@parallaxmail.com>
2021-04-19 06:51:31 -0400

    Clean up Pico Mouse Shake.

--- pico/mouseshake/circuitpython/code.py
+++ pico/mouseshake/circuitpython/code.py
@@ -1,4 +1,4 @@
-# MouseShake is a USS mouse the shakes the mouse pointer when active! This is a hardware solution to 
+# MouseShake is a USS mouse the shakes the mouse pointer when active! This is a hardware solution to 
 # keeping a computer from sleeping.  
 # CirtvuitPython - Raspberry Pi Pico
 
@@ -17,7 +17,7 @@ loop_sleep = .5
 
 # Define components
 # TODO: Adjust pin definitions for your board layout
-led = digitalio.DigitalInOut(board.GP14)
+led = digitalio.DigitalInOut(board.GP12)
 led.direction = digitalio.Direction.OUTPUT
 button = digitalio.DigitalInOut(board.GP13)
 button.switch_to_input(pull=digitalio.Pull.DOWN)
@@ -50,56 +50,3 @@ while True:
             led.value = False
     else:
         time.sleep(loop_sleep)
-       # MouseShake is a USS mouse the shakes the mouse pointer when active! This is a hardware solution to 
-# keeping a computer from sleeping.  
-# CirtvuitPython - Raspberry Pi Pico
-
-# Libraries
-import time
-import board
-import digitalio
-import usb_hid
-from adafruit_hid.mouse import Mouse
-
-# User settings
-max_shake_minutes = 240
-move_distance = 100
-mouse_pause = .5
-loop_sleep = .5
-
-# Define components
-# TODO: Adjust pin definitions for your board layout
-led = digitalio.DigitalInOut(board.GP14)
-led.direction = digitalio.Direction.OUTPUT
-button = digitalio.DigitalInOut(board.GP13)
-button.switch_to_input(pull=digitalio.Pull.DOWN)
-mouse = Mouse(usb_hid.devices)
-
-# Inititalize variables
-active_state = False;
-active_time = 0;
-
-# Initial LED state
-led.value = False
-
-while True:
-    if button.value:
-        active_state = not active_state
-        if active_state:
-            led.value = True
-            active_time = time.time()
-        else:
-            led.value = False
-    if active_state:
-        mouse.move(x=move_distance)
-        time.sleep(mouse_pause)
-        mouse.move(x=-1 * move_distance)
-        time.sleep(mouse_pause)
-        elapsed = time.time()- active_time
-        if elapsed >= (max_shake_minutes * 60):
-            active_state = False
-            active_time = 0
-            led.value = False
-    else:
-        time.sleep(loop_sleep)
-       
